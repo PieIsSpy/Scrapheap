f303abc081d22be6137344cd00d68bdb8cb8584a
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-15 14:42:03 +0800

    feat: rearranged input vectors, removed void in _physics_process

--- entities/player/player_movement.gd
+++ entities/player/player_movement.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-    var input_direction = Input.get_vector("Up", "Down", "Left", "Right")
-    velocity = input_direction * speed
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = input_direction * speed
 
-func _physics_process(delta: float) -> void:
-    get_input()
-    move_and_slide()
+func _physics_process(delta: float):
+	get_input()
+	move_and_slide()
