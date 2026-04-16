d8fc3e6088bc1faf7cb7cb3b8f950776a2d7742f
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-16 17:28:58 +0800

    feat: moved camera into its own component scene, made the camera follow the cursor instead of the player

---
+++ components/camera/camera_2d.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+const dead_zone = 160
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var _target = event.position - get_viewport().size * 0.5
+		if _target.length() < dead_zone:
+			self.position = Vector2(0,0)
+		else:
+			self.position = _target.normalized() * (_target.length() - dead_zone) * 0.5
