494596e407cf1b2bb07ed10cf1f8929df30b0489
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-19 21:33:57 +0800

    feat: made the gun follow the mouse

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-@onready var gun = $Gun
-@onready var RotationOffset: Node2D = $Gun/RotationOffset
-@onready var SpriteShadow: Sprite2D = $Gun/RotationOffset/Sprite2D/shadow
+@onready var Gun = $Gun
+@onready var PivotMarker: Marker2D = $PivotMarker
 
 @export var speed = 400
 
 func _process(delta: float) -> void:
-	RotationOffset.rotation = lerp_angle(RotationOffset.rotation, (get_global_mouse_position() - global_position).angle(), 6.5 * delta)
-	SpriteShadow.position = Vector2(-2, 2).rotated(-RotationOffset.rotation)
+	var mouse_dir := PivotMarker.global_position.direction_to(get_global_mouse_position())
+	Gun.global_position = PivotMarker.global_position + mouse_dir * 60
+
+	Gun.scale.y = 1 if mouse_dir.x > 0 else -1
+
+	Gun.look_at(get_global_mouse_position())
+	Gun.rotate(deg_to_rad(30 if mouse_dir.x > 0 else -30))
 
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
