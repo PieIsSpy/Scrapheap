d63256ad27980706803e76c68da00901d29fe4bf
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-20 16:41:18 +0800

    feat: migrated spawner script to its own folder

--- components/spawner/spawner.gd
+++ components/spawner/spawner.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+const spider = preload("res://entities/spider/spider.tscn")
+const min_dist = 661
+const max_dist = 1000
+
+func _on_timer_timeout() -> void:
+	var angle = randf_range(0, 360)
+	var distance = randf_range(min_dist, max_dist)
+
+	var spawn_offset = Vector2(
+		cos(deg_to_rad(angle)) * distance,
+		sin(deg_to_rad(angle)) * distance
+	)
+
+	var new_enemy = spider.instantiate()
+	new_enemy.global_position = $"../Player".global_position + spawn_offset
+	add_child(new_enemy)
